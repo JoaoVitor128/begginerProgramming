ae414a0cf77c997b2f00bdc0faff778061e00c27
JoaoVitor128 <joaovitorbp128@gmail.com>
2026-02-11 14:07:24 -0300

    ADD: Melhor navegação dos módulos

--- src/components/ClassList.jsx
+++ src/components/ClassList.jsx
@@ -1,8 +1,13 @@
 import './List.css'
+import {useState} from "react"
 import { useParams, Link } from 'react-router-dom'
 import classDb from '../db/class'
 
 const ClassList = () => {
+    const [selectedMod, setSelectedMod] = useState(0)
+
+    console.log(classDb[selectedMod]);
+    
 
     return (
         <div className='classList'>
@@ -11,20 +16,24 @@ const ClassList = () => {
 
             <h3><Link to={"https://drive.google.com/drive/folders/1rHe7dAMbn3WcSJBfowYSrsr872p0pZlD"} className='driveLink'> Acesse as aulas no Google Drive</Link></h3>
 
+            <div className="modSelection list">
+                {classDb.map((mod)=>(
+                    <button onClick={()=> setSelectedMod(classDb.indexOf(mod))}>Módulo {classDb.indexOf(mod)} - {mod[0]}</button>
+                ))}
+            </div>
+
             <div className="list">
-                {classDb.map((mod) => (
-                    <div key={mod.id} className="classItem">
-                        <h1>{mod[0]}</h1>
+                    <div key={selectedMod} className="classItem">
+                        <h1>{classDb[selectedMod][0]}</h1>
                         <div className="modClass">
-                            {mod.slice(1).map((lesson, i) => (
-                                <Link className={`${lesson.href === "" ? "unavailable" : ""}`} key={i} to={`/class/${classDb.indexOf(mod)}/${i + 1}`} rel="noopener noreferrer">
+                            {classDb[selectedMod].slice(1).map((lesson, i) => (
+                                <Link className={`${lesson.href === "" ? "unavailable" : ""}`} key={i} to={`/class/${classDb.indexOf(classDb[selectedMod])}/${i + 1}`} rel="noopener noreferrer">
                                     <span>Aula {i + 1}</span>
                                     {lesson.title}
                                 </Link>
                             ))}
                         </div>
                     </div>
-                ))}
             </div>
         </div>
     )
